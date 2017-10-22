bc375df29e9e01b43ff201efa2c7d7a896d83c6f
Yan <Yan@MacBook-Pro-de-User.local>
2017-10-22 15:09:05 -0500

    set email nad production host

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default :from => "gerencia@stcarga.com"
+  default :from => ENV['EMAIL_FROM']
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
