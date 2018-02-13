875cde5c039039e37402cbbc1d679ff06d0ea5ca
Yan <yancarlosmarinosorio@gmail.com>
2018-02-13 10:56:29 -0600

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -103,4 +104,6 @@ Rails.application.configure do
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
   }
+  config.assets.compile = true
+  config.assets.digest = true
 end
